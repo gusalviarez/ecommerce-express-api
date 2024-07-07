bc0f5ee3e57b108a61b1e26614fc6015b03ec484
gusalviarez <gusalv2001@gmail.com>
2024-07-07 13:09:15 -0400

    add products creation by id

--- src/products.routes.js
+++ src/products.routes.js
@@ -2,10 +2,25 @@ import { Router } from "express";
 
 const router = Router();
 
+// get products
 export const getProducts = async (req, res) => {
   res.status(200).send("hello");
 };
 
+// get a product
+
+export const getUserById = async (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send("got product with id: " + id);
+};
+
+// create an user
+
+// update product
+
+// delete by id
+
 router.get("/products", getProducts);
+router.get("/products/:id", getUserById);
 
 export default router;
