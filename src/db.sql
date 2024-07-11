117e5f49779c8c1f405809dac5a631986a02811b
gusalviarez <gusalv2001@gmail.com>
2024-07-10 23:45:17 -0400

    add database integration

---
+++ src/db.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE ecommercedb;
+
+USE ecommercedb;
+
+CREATE TABLE products(
+  id BINARY(16) PRIMARY KEY DEFAULT UUID(),
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  description TEXT,
+  category VARCHAR(255)
+);
+
+CREATE TABLE categories(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+INSERT INTO products (name, price, description, category) VALUES
+  ('Men's T-Shirt', 19.99, 'A comfortable and stylish T-shirt for men.', 'Men'),
+  ('Women's Sneakers', 49.95, 'Stylish and comfortable sneakers for women.', 'Shoes');
+
+INSERT INTO categories (name) VALUES
+  ('Men'),
+  ('Shoes');
+
+
+CREATE TABLE product_categories (
+  product_id BINARY(16) REFERENCES products(id),
+  category_id INT REFERENCES categories(id),
+  PRIMARY KEY (product_id, category_id),
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+
--- src/products.routes.js
+++ src/products.routes.js
@@ -2,25 +2,46 @@ import { Router } from "express";
 
 const router = Router();
 
-// get products
 export const getProducts = async (req, res) => {
   res.status(200).send("hello");
 };
 
-// get a product
-
-export const getUserById = async (req, res) => {
+export const getProductById = async (req, res) => {
   const id = parseInt(req.params.id);
   res.send("got product with id: " + id);
 };
 
-// create an user
+export const createProduct = async (req, res) => {
+  try {
+    const { name } = req.body;
+    res.status(201).send(name);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
-// update product
+export const updateProduct = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
 
-// delete by id
+  // NOTE: id to find user, name to update user
+
+  return res.send("new name " + name);
+};
+
+export const deleteProduct = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  // NOTE: add db interaction
+
+  // NOTE:  check if it find the user
+  return res.sendStatus(204);
+};
 
 router.get("/products", getProducts);
-router.get("/products/:id", getUserById);
+router.get("/products/:id", getProductById);
+router.post("/products", createProduct);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
